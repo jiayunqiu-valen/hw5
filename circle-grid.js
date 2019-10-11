fb26733bdbd154a8956a41f108ba560bb0923948
jiayunqiu-valen <55211937+jiayunqiu-valen@users.noreply.github.com>
2019-10-11 15:40:35 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  
+  for (var x = 10; x< width; x =x + 20) {
+    for(var y = 10; y< height; y =y + 20) {
+    fill(255);
+    ellipse(x,y,15,15);
+    
+    }
+    
+  }
+}
